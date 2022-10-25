b2e41718138b2a22d76f58757a2c8efa24d2207b
Malikan Volga <67208480+MalikanVolga@users.noreply.github.com>
2022-10-25 15:44:32 -0700

    added animations

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,19 +1,50 @@
 /** @format */
 
-import React from "react"
+import React, { useEffect } from "react"
 import { InlineWidget } from "react-calendly"
 import elvis from "../../assets/headshots/elvis.jpg"
 import { Element } from "react-scroll"
-
+import AOS from "aos"
+import "aos/dist/aos.css"
 const Contact = () => {
+	useEffect(() => {
+		AOS.init({
+			// Global settings:
+			disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+			startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
+			initClassName: "aos-init", // class applied after initialization
+			animatedClassName: "aos-animate", // class applied on animation
+			useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+			disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+			debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+			throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+			// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+			offset: 120, // offset (in px) from the original trigger point
+			delay: 0, // values from 0 to 3000, with step 50ms
+			duration: 400, // values from 0 to 3000, with step 50ms
+			easing: "ease", // default easing for AOS animations
+			once: false, // whether animation should happen only once - while scrolling down
+			mirror: false, // whether elements should animate out while scrolling past them
+			anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+		})
+	})
 	return (
 		<>
 			<Element name='Contact' className='element'>
-				<div className='text-white p-10  bg-gradient-to-b from-primary-100 to-primary-300'>
+				<div
+					data-aos='fade-up'
+					data-aos-delay='50'
+					data-aos-duration='1000'
+					data-aos-easing='ease-in-out'
+					data-aos-mirror='true'
+					data-aos-once='false'
+					data-aos-anchor-placement='top-center'
+					className='text-white p-10  bg-gradient-to-b from-primary-100 to-primary-300'>
 					<h1 className='text-secondary-100 text-center text-3xl  font-bold '>
 						CONTACT US
 					</h1>
-					<div className='max-w-lg w-full h-[3px] mx-auto my-20 bg-secondary-100'></div>
+					<div className='w-[100px] h-[3px] mx-auto my-20 bg-secondary-100'></div>
 
 					<div className=' grid lg:grid-cols-4 lg:gap-10  lg:max-w-6xl m-auto'>
 						<div className='text-center my-auto lg:col-span-2 lg:order-2'>
