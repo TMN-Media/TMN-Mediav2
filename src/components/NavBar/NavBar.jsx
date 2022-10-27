ee65e86df3383452c9e194bc74dc5d08ee0a00fe
Malikan Volga <67208480+MalikanVolga@users.noreply.github.com>
2022-10-27 10:32:33 -0700

    capitalization and footer image clarity

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -15,39 +15,39 @@ const NavBar = () => {
 	}
 	return (
 		<div
-			className=' z-50 absolute  top-0 w-full h-screen 
+			className='lg:flex justify-between  z-50 absolute  top-0 w-full h-screen 
 		text-white text-center'>
 			<div
-				className={`flex justify-between transition-all ease-in-out duration-700 ${
+				className={`flex justify-between lg:w-max lg:block transition-all ease-in-out duration-700 ${
 					toggle ? "bg-primary-100 bg-opacity-30 backdrop-blur-lg " : ""
 				}`}>
 				<img
 					src={logo}
 					alt=''
-					className='w-[200px]  relative top-0 mt-2 ml-2 p-2 '
+					className='w-[200px]  relative top-0 mt-2 ml-2 p-2 lg:h-max'
 				/>
 				{toggle ? (
 					<TfiClose
-						className='text-[44px] p-2 mt-5 mr-1 cursor-pointer'
+						className='text-[44px] p-2 mt-5 mr-1 cursor-pointer lg:hidden'
 						onClick={toggleOnMenu}
 					/>
 				) : (
 					<CgMenuGridO
-						className='text-[50px] p-2 mt-5 mr-2 cursor-pointer'
+						className='text-[50px] p-2 mt-5 mr-2 cursor-pointer lg:hidden'
 						onClick={toggleOnMenu}
 					/>
 				)}
 			</div>
 
 			<ul
-				className={` absolute bg-primary-100 bg-opacity-30 backdrop-blur-lg transition-all ease duration-700 flex flex-col justify-center h-1/4 w-full z-10 ${
+				className={` absolute lg:mr-2 bg-primary-100 lg:p-6  lg:bg-inherit lg:backdrop-blur-none bg-opacity-30 backdrop-blur-lg transition-all ease duration-700 flex flex-col lg:flex-row lg:relative lg:opacity-100 lg:justify-end justify-center h-1/4 lg:h-max lg:w-1/2 w-full z-10 ${
 					toggle ? " opacity-100  " : "   opacity-0  "
 				}`}>
 				{menuItems.map((menuItem, index) => {
 					return (
 						<li className=' m-3  ' key={index}>
 							<Link
-								className=' hover:text-secondary-100 font-bold uppercase cursor-pointer'
+								className=' hover:text-secondary-100 font-bold uppercase cursor-pointer lg:text-xl'
 								to={menuItem.name}
 								spy={true}
 								smooth={true}
