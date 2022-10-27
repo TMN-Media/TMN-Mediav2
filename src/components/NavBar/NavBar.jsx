9a030c7ff988a652a809e4cf34eb6270ae9a172a
Malikan Volga <67208480+MalikanVolga@users.noreply.github.com>
2022-10-27 11:01:23 -0700

    capitalization and footer image clarity

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -15,7 +15,7 @@ const NavBar = () => {
 	}
 	return (
 		<div
-			className='lg:flex justify-between  z-50 absolute  top-0 w-full h-screen 
+			className='lg:flex  justify-between  z-50 absolute  top-0 w-full h-screen lg:h-max
 		text-white text-center'>
 			<div
 				className={`flex justify-between lg:w-max lg:block transition-all ease-in-out duration-700 ${
@@ -24,7 +24,7 @@ const NavBar = () => {
 				<img
 					src={logo}
 					alt=''
-					className='w-[200px]  relative top-0 mt-2 ml-2 p-2 lg:h-max'
+					className='w-[200px] lg:w-[300px]  relative top-0 mt-2 ml-2 p-2 lg:h-max'
 				/>
 				{toggle ? (
 					<TfiClose
@@ -40,7 +40,7 @@ const NavBar = () => {
 			</div>
 
 			<ul
-				className={` absolute lg:mr-2 bg-primary-100 lg:p-6  lg:bg-inherit lg:backdrop-blur-none bg-opacity-30 backdrop-blur-lg transition-all ease duration-700 flex flex-col lg:flex-row lg:relative lg:opacity-100 lg:justify-end justify-center h-1/4 lg:h-max lg:w-1/2 w-full z-10 ${
+				className={` absolute lg:mr-2 bg-primary-100 lg:py-10  lg:bg-inherit lg:backdrop-blur-none bg-opacity-30 backdrop-blur-lg transition-all ease duration-700 flex flex-col lg:flex-row lg:relative lg:opacity-100 lg:justify-end justify-center h-1/4 lg:h-max lg:w-1/2 w-full z-10 ${
 					toggle ? " opacity-100  " : "   opacity-0  "
 				}`}>
 				{menuItems.map((menuItem, index) => {
